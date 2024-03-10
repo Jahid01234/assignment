ad9cc38c476bb46fde765a1aa1264ff988979cfe
Jahid01234 <mdjahidhasan790523@gmail.com>
2024-03-10 12:48:22 +0600

    completed

---
+++ bin/assignment.dart
@@ -0,0 +1,31 @@
+// The main function
+void main() {
+  Media media = Media();
+  Song song = Song("Jahid");
+
+  media.play();
+  song.play();
+}
+
+
+
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+
